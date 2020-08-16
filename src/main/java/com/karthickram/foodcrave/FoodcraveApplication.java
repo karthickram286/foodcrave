faaaa88b47ed4bf49eb87c3629214271d12e378c
Karthick R <karthick.ra@samsung.com>
2020-08-16 20:23:47 +0530

    first commit

---
+++ src/main/java/com/karthickram/foodcrave/FoodcraveApplication.java
@@ -0,0 +1,13 @@
+package com.karthickram.foodcrave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcraveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcraveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthickram/foodcrave/FoodcraveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthickram.foodcrave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcraveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
